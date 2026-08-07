fceecaa59dea64f03f78405a25604c0a8eaf5c12
Deepu1007 <hunterb005king@gmail.com>
2026-08-07 23:27:10 +0530

    fixed indentation

--- banking_system/wsgi.py
+++ banking_system/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for banking_system project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
 from django.core.wsgi import get_wsgi_application
 import os
 
@@ -13,9 +5,24 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'banking_system.settings')
 
 application = get_wsgi_application()
 
-# 👇 ADD THIS
+# ✅ Run migrations automatically
 try:
     from django.core.management import call_command
     call_command('migrate')
 except Exception as e:
     print("Migration error:", e)
+
+# ✅ Create superuser automatically
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+try:
+    if not User.objects.filter(username='deepu').exists():
+        User.objects.create_superuser(
+            username='deepu',
+            email='hunterb005king@gmail.com',
+            password='Hunter@0852'
+        )
+        print("Superuser created")
+except Exception as e:
+    print("Superuser error:", e)
